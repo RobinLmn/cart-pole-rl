f78aa712a5f5a018451e324cae508bb1dc3795d3
Robin Leman <robin.leman@mail.mcgill.ca>
2025-07-13 11:20:09 -0700

    Adds a tanh activation function

--- source/rl/activation_functions.cpp
+++ source/rl/activation_functions.cpp
@@ -2,16 +2,20 @@
 
 #include "core/log.hpp"
 
-activation_function_signature relu = [](const Eigen::MatrixXf& x) { return x.array().cwiseMax(0.0f); };
-activation_function_signature relu_derivative = [](const Eigen::MatrixXf& x) { return (x.array() > 0.0f).cast<float>(); };
+static const activation_function_signature fn_relu = [](const Eigen::MatrixXf& x) { return x.array().cwiseMax(0.0f); };
+static const activation_function_signature fn_relu_derivative = [](const Eigen::MatrixXf& x) { return (x.array() > 0.0f).cast<float>(); };
 
-activation_function_signature identity = [](const Eigen::MatrixXf& x) { return x; };
-activation_function_signature identity_derivative = [](const Eigen::MatrixXf& x) { return Eigen::MatrixXf::Ones(x.rows(), x.cols()); };
+static const activation_function_signature fn_identity = [](const Eigen::MatrixXf& x) { return x; };
+static const activation_function_signature fn_identity_derivative = [](const Eigen::MatrixXf& x) { return Eigen::MatrixXf::Ones(x.rows(), x.cols()); };
+
+static const activation_function_signature fn_tanh = [](const Eigen::MatrixXf& x) { return x.array().tanh(); };
+static const activation_function_signature fn_tanh_derivative = [](const Eigen::MatrixXf& x) { const Eigen::MatrixXf& tanh_x = x.array().tanh(); return 1.0f - tanh_x.array().square(); };
 
 std::unordered_map<std::string, activation_function> activation_name_map = 
 {
-    { "relu", { "relu", relu, relu_derivative } },
-    { "identity", { "identity", identity, identity_derivative } }
+    { "relu", { "relu", fn_relu, fn_relu_derivative } },
+    { "identity", { "identity", fn_identity, fn_identity_derivative } },
+    { "tanh", { "tanh", fn_tanh, fn_tanh_derivative } }
 };
 
 activation_function::activation_function(const char* name, const activation_function_signature& func, const activation_function_signature& derivative)
